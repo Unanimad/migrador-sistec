523877a48331cc5ea4407d130d26c4c52fc6b8de
Raphael F <raphaelf.ti@gmail.com>
2025-02-10 13:04:50 -0300

    add models sistec + migrations

--- apps/sistec/models.py
+++ apps/sistec/models.py
@@ -6,22 +6,25 @@ from apps.sigaa.models.public import SigCurso
 
 class Instituicao(models.Model):
     codigo = models.CharField("Código", max_length=10, unique=True)
-    sig_unidade = models.ForeignKey(SigCampus, models.DO_NOTHING)
+    descricao = models.CharField("Descrição", max_length=255)
+    # sig_unidade = models.ForeignKey(SigCampus, models.DO_NOTHING, null=True)
 
     class Meta:
-        abstract = True
+        db_table = "sistec_instituicao"
 
 
 class Curso(models.Model):
     id_sistec = models.PositiveIntegerField("ID SISTEC", unique=True)
-    sig_curso = models.ForeignKey(SigCurso, models.DO_NOTHING)
+    descricao = models.CharField("Descrição", max_length=255)
+    instituicao = models.ForeignKey(Instituicao, models.DO_NOTHING)
+    # sig_curso = models.ForeignKey(SigCurso, models.DO_NOTHING, null=True)
 
     class Meta:
-        abstract = True
+        db_table = "sistec_curso"
 
 
 class CicloMatricula(models.Model):
-    instituicao = models.ForeignKey('Instituicao', models.DO_NOTHING)
+    instituicao = models.ForeignKey(Instituicao, models.DO_NOTHING)
 
     class Meta:
-        abstract = True
+        db_table = "sistec_ciclo_matricula"
